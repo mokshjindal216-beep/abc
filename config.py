86cb123f356e1a02b6b64226eae7e23eb7581d51
mokshjindal216-beep <mokshjindal216@gmail.com>
2025-12-21 20:01:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# config.py
+import os
+
+# The script will read these from the GitHub Secrets (Environment Variables)
+# Do NOT hardcode your keys here for the public file.
+
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+GNEWS_API_KEY = os.getenv("GNEWS_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
+
+IG_USER_ID = os.getenv("IG_USER_ID")
+IG_ACCESS_TOKEN = os.getenv("IG_ACCESS_TOKEN")
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_ADMIN_ID = os.getenv("TELEGRAM_ADMIN_ID")
